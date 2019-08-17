4e1c58ba3202878fd01d5de534a92d1ba2221be1
krishnaraj8991 <krishnarajs078@gmail.com>
2019-08-17 16:14:37 +0530

    file transfer using flask

--- mp/webpage/app.py
+++ mp/webpage/app.py
@@ -1,41 +1,15 @@
 # from flask import Flask
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
-import mp
-import threading
+# import threading
 
 
-mplayer=None
-mplayer = threading.Thread(target=mp.player, args=())
-mplayer.daemon=True
-   
-mplayer.start()
    
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello world'
-
-
-@app.route('/player', methods = ['GET', 'POST'])
-def upload():
-   mp.play()
-   return render_template('upload.html')
-
-@app.route('/response', methods = ['GET', 'POST'])
-def input():
-   # play/pause=request.args.get("pplay/pause")
-   mp.pause()
-   return render_template('upload2.html')
-
-@app.route('/exit', methods = ['GET', 'POST'])
-def input1():
-   # play/pause=request.args.get("pplay/pause")
-   global mplayer
-   mp.pause()
-   mp.exit()
-   return ("thanks for comming")
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
