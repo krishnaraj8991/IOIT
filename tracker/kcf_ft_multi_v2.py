4eee83788b7f7c0dab1f490beec098e07d32cad9
krishnaraj8991 <krishnarajs078@gmail.com>
2019-08-18 17:19:24 +0530

    added feature to track multiple faces over time

--- tracker/kcf_ft_multi_v2.py
+++ tracker/kcf_ft_multi_v2.py
@@ -2,36 +2,56 @@ import cv2
 import sys
 import numpy as np
 
-def face_detect(video):
+def face_detect(video,pretraker,mode):
+    """
+    mode=> 1 for lost trackers
+           2 for check for new faces
+           3 lost some trackers
+    """
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     flag = True
     bbox = (287, 23, 86, 320)
-    
+    print("mode:-"+str(mode))
     trackers=[]
-    
-    
-    
+        
     while flag:
         ok, frame = video.read()
-        # cv2.imshow('img',frame)
+        if pretraker !=None and mode==2:
+            for tracker in pretraker:
+                ok, bbox = tracker.update(frame)
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                cv2.rectangle(frame, p1, p2, (0,0,0), -1, 1)
+                
+                
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('img',gray)
+        
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
             tracker = cv2.TrackerKCF_create()
             bbox=(x,y,w,h)
             ok = tracker.init(frame, bbox)    
             trackers.append(tracker)
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,255),2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = frame[y:y+h, x:x+w]
             flag = False
             # break
         cv2.imshow('face_detect',frame)
         k = cv2.waitKey(1) & 0xff
-    
+        if ((pretraker !=None and mode !=1) or mode==2)  :
+            flag=False
             # k = cv2.waitKey(0)
-    return trackers
-
+    if trackers:
+        print("new tracker")
+        if mode ==2:
+            for tracker in pretraker:
+                trackers.append(tracker)
+        return trackers
+    else:
+        print("old tracker") 
+        return pretraker
 if __name__ == '__main__' :
     
     # tracker = cv2.TrackerKCF_create()
@@ -53,9 +73,10 @@ if __name__ == '__main__' :
 
     # detectes face and generates a tracker for it
     # -----------------------------------------------
-    trackers=face_detect(video)
+    trackers=face_detect(video,None,1)
     # -----------------------------------------------
     flag= False
+    cnt=0
     while True:
         # Read a new frame
         ok, frame = video.read()
@@ -66,10 +87,6 @@ if __name__ == '__main__' :
         timer = cv2.getTickCount()
  
         # Update tracker
-        #single tracker 
-        # ok, bbox = tracker.update(frame)
-        
-        # multi tracker
         bboxes=[]
         for tracker in trackers:
             ok, bbox = tracker.update(frame)
@@ -78,30 +95,20 @@ if __name__ == '__main__' :
         # Calculate Frames per second (FPS)
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
  
-        # Draw bounding box
-        # if ok:
-        #     # Tracking success
-        #     p1 = (int(bbox[0]), int(bbox[1]))
-        #     p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-        #     cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-        # else :
-        #     # Tracking failure
-        #     cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
-        #     # print("used ")
-        #     flag=True
-        #
-        # multi bbox 
         for bbox in bboxes:
                 # Tracking success
                 p1 = (int(bbox[0]), int(bbox[1]))
                 p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-                cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+                cv2.rectangle(frame, p1, p2, (255,255,0), 3, 1)
         if len(bboxes)==0 :
             # Tracking failure
             cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
             # print("used ")
             flag=True
-        
+        if cnt>50:
+            flag=True
+            cnt=100
+            # cnt=0
         # Display tracker type on frame
         cv2.putText(frame, "KCF_face_tracker" + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2)
      
@@ -111,14 +118,23 @@ if __name__ == '__main__' :
         # Display result
         cv2.imshow("Tracking", frame)
         if flag:
-            # use haar_cascade to find the face again
-            # and reset the tracker
-            # single tracker
-            # tracker=face_detect(video)
-            # multi tracker
-            trackers=face_detect(video)
+            """
+                use haar_cascade to find the face again
+                and reset the tracker
+                single tracker
+                tracker=face_detect(video)
+                multi tracker
+            """
+            if cnt==100:
+                trackers=face_detect(video,trackers,2)
+            else:
+                trackers=face_detect(video,trackers,1)
+            cnt=0
+            # print(len(trackers))
+            print("trackers:-"+str(len(trackers)))
             
             flag= False
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
         if k == 27 : break
+        cnt+=1
