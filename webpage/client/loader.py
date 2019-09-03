49baf1cbd02d7f79ebba3c1262b89d76db436d3e
krishnaraj8991 <krishnarajs078@gmail.com>
2019-09-03 14:02:03 +0530

    function for uploading

--- webpage/client/loader.py
+++ webpage/client/loader.py
@@ -1,13 +1,23 @@
 import requests 
-  
-url ="http://127.0.0.1:5000/uploader"
-# data ="D:\coding\py\webpage\data\img.jpg"
-files = {'file': open('data\img.jpg', 'rb')}
-# data=""
-# sending post request and saving response as response object 
-r = requests.post(url = url, files=files) 
-# r = requests.get(url = url) 
-  
-# extracting response text  
-pastebin_url = r.text 
-print("The pastebin URL is:%s"%pastebin_url)
+
+
+def send(data):
+    url ="http://127.0.0.1:5000/uploader"
+    files = {'file': open(str('data\\'+data), 'rb')}
+    r = requests.post(url = url, files=files) 
+    pastebin_url = r.text 
+    print(pastebin_url)
+    
+if __name__ == "__main__":
+    url ="http://127.0.0.1:5000/uploader"
+    # data ="D:\coding\py\webpage\data\img.jpg"
+    files = {'file': open('data\img.jpg', 'rb')}
+    # data=""
+    # sending post request and saving response as response object 
+    r = requests.post(url = url, files=files) 
+    # r = requests.get(url = url) 
+    
+    # extracting response text  
+    pastebin_url = r.text 
+    print("The pastebin URL is:%s"%pastebin_url)
+    pass 
